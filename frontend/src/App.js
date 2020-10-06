c84c921b08b2290e100bbf4046a6bc4779be7652
Melvin <42515663+Melvinmcrn@users.noreply.github.com>
2020-10-06 23:46:46 +0700

    [Frontend] Shorten url UI (#1)
    * create box for input link
    
    * add navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Navbar } from "react-bootstrap";
+
+import { Home } from "views";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark" sticky="top">
+        <Navbar.Brand href="#home">
+          idk.ly
+        </Navbar.Brand>
+      </Navbar>
+      <Home />
     </div>
   );
 }
