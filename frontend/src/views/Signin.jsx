e770ba4bb10f4ac561baa6bca24cf643c9b15a3b
Melvin Macaranas <greatwinblowaway@gmail.com>
2020-10-26 16:14:16 +0700

    add all input for signup form

--- frontend/src/views/Signin.jsx
+++ frontend/src/views/Signin.jsx
@@ -10,20 +10,19 @@ class Signin extends React.Component {
         <div className="form-container">
           <div className="form-card">
             <Form>
-              <Form.Group controlId="formBasicEmail">
+              <Form.Group controlId="email">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
               </Form.Group>
 
-              <Form.Group controlId="formBasicPassword">
+              <Form.Group controlId="username">
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="text" placeholder="Enter username" />
+              </Form.Group>
+
+              <Form.Group controlId="password">
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-              <Form.Group controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
               </Form.Group>
               <Button variant="primary" type="submit">
                 Submit
