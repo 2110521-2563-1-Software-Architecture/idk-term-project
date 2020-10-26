b91c89ae7deb51182cc617ffc9b0398ab741861b
KritKhan <kridsana.khunkaew@gmail.com>
2020-10-26 16:57:53 +0700

    onchage

--- frontend/src/views/Signup.jsx
+++ frontend/src/views/Signup.jsx
@@ -1,40 +1,69 @@
-import React from "react"
-import { Form, Button } from "react-bootstrap"
+import React from "react";
+import { Form, Button } from "react-bootstrap";
 
 import "styles/Signup.scss";
 
 class Signup extends React.Component {
+  state = {
+    signupData: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  };
+
+  handleSignupDataChange = ({ target: { name, value } }) => {
+    const signupData = this.state.signupData;
+    signupData[name] = value;
+    this.setState({ signupData });
+  };
+
   render() {
-  	return(
-			<div className="signup-container">
-				<div className="form-container">
-					<div className="form-card">
-						<Form>
+    return (
+      <div className="signup-container">
+        <div className="form-container">
+          <div className="form-card">
+            <Form>
               <Form.Group controlId="formBasicUsername">
                 <Form.Label>Username</Form.Label>
-                <Form.Control type="username" placeholder="Enter username" />
+                <Form.Control
+                  type="username"
+                  placeholder="Enter username"
+                  name="username"
+                  onChange={this.handleSignupDataChange}
+                />
               </Form.Group>
 
-							<Form.Group controlId="formBasicEmail">
-								<Form.Label>Email address</Form.Label>
-								<Form.Control type="email" placeholder="Enter email" />
-							</Form.Group>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  name="email"
+                  onChange={this.handleSignupDataChange}
+                />
+              </Form.Group>
 
-							<Form.Group controlId="formBasicPassword">
-								<Form.Label>Password</Form.Label>
-								<Form.Control type="password" placeholder="Password" />
-							</Form.Group>
-              
-              <div className="btn-rigth">
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  onChange={this.handleSignupDataChange}
+                />
+              </Form.Group>
+
+              <div className="content-right">
                 <Button variant="primary" type="submit">
                   Sign up
                 </Button>
               </div>
-						</Form>
-					</div>
-				</div>
-			</div>
-    )
+            </Form>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
