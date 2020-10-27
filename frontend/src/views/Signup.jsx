5622f4bcbd0778b99e482ac7896cbfe198e3d6f9
KritKhan <kridsana.khunkaew@gmail.com>
2020-10-27 16:35:23 +0700

    handle submit

--- frontend/src/views/Signup.jsx
+++ frontend/src/views/Signup.jsx
@@ -6,9 +6,9 @@ import "styles/Signup.scss";
 class Signup extends React.Component {
   state = {
     signupData: {
-      username: "",
-      email: "",
+      user_name: "",
       password: "",
+      user_email: "",
     },
   };
 
@@ -18,18 +18,36 @@ class Signup extends React.Component {
     this.setState({ signupData });
   };
 
+  handleSubmit = (e) => {
+    e.prevenDefault();
+
+    axios
+      .post(process.env.REACT_APP_BACKEND_URL + "/auth/register", this.state.signupData)
+      .then( (response) => {
+        console.log(response);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        if (error.response && error.response.status === 400) {
+          
+        } else {
+
+        }
+      });
+  };
+
   render() {
     return (
       <div className="signup-container">
         <div className="form-container">
           <div className="form-card">
-            <Form>
+            <Form onSubmit={this.handleSubmit}>
               <Form.Group controlId="formBasicUsername">
                 <Form.Label>Username</Form.Label>
                 <Form.Control
                   type="username"
                   placeholder="Enter username"
-                  name="username"
+                  name="user_name"
                   onChange={this.handleSignupDataChange}
                 />
               </Form.Group>
@@ -39,7 +57,7 @@ class Signup extends React.Component {
                 <Form.Control
                   type="email"
                   placeholder="Enter email"
-                  name="email"
+                  name="user_email"
                   onChange={this.handleSignupDataChange}
                 />
               </Form.Group>
