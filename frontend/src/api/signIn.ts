9a7192786553cedaf79555038b813ff2552f1e54
Melvin Macaranas <greatwinblowaway@gmail.com>
2020-10-27 17:52:16 +0700

    add signin

--- frontend/src/api/signIn.ts
+++ frontend/src/api/signIn.ts
@@ -1,5 +1,24 @@
 import client from "./httpClient";
 
-export const signIn = () => {
-  client.get("token/").then().catch();
+export interface props {
+  usernameEmail: string;
+  password: string;
+}
+
+export const signIn = (props: props) => {
+  const body = {
+    user_name: props.usernameEmail,
+    password: props.password,
+  };
+  client
+    .post("token/", body)
+    .then((response) => {
+      if (response.status === 200) {
+        const token = `Bearer ${response.data.access}`;
+        localStorage.setItem("token", token);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
